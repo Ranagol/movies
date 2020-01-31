f3a75ddc837fcb0701232b9b7694bd81da69a0e8
Andor <andorhorvat@gmail.com>
2020-01-31 21:07:54 +0100

    Zadatak 5

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'Show')
 @section('content')
   <h2>{{ $movie->title }}</h2>
+  <p>{{ $movie->genre }}</p>
+  <p>{{ $movie->director }}</p>
+  <p>{{ $movie->year }}</p>
+  <p>{{ $movie->storyline }}</p>
+  
 
 @endsection
 
